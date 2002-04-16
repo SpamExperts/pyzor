7e41106c23e5abddaa8d69aea266d6270775d97e
Frank Tobin <ftobin@neverending.org>
2002-04-16 18:17:02 +0000

    to help xemacs with highlighting, move the copyright stuff into a comment, instead of docstring

--- pyzor/lib/pyzor/server.py
+++ pyzor/lib/pyzor/server.py
@@ -1,21 +1,21 @@
-"""networked spam-signature detection server
+"""networked spam-signature detection server"""
 
-Copyright (C) 2002 Frank J. Tobin <ftobin@neverending.org>
+# Copyright (C) 2002 Frank J. Tobin <ftobin@neverending.org>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, visit the following URL:
+# http://www.gnu.org/copyleft/gpl.html
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, visit the following URL:
-http://www.gnu.org/copyleft/gpl.html
-"""
 
 from __future__ import division
 
@@ -31,7 +31,7 @@ from pyzor import *
 
 __author__   = pyzor.__author__
 __version__  = pyzor.__version__
-__revision__ = "$Id: server.py,v 1.7 2002-04-14 22:23:08 ftobin Exp $"
+__revision__ = "$Id: server.py,v 1.8 2002-04-16 18:17:02 ftobin Exp $"
 
 
 class Record(object):
