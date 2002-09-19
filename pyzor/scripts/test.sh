ebbd9b30697d7fc5b967ffe589a68ea35668292d
Frank Tobin <ftobin@neverending.org>
2002-09-19 01:56:46 +0000

    *   pyzord no longer daemonizes itself, and now writes         it logging to standard output.
        *   The following server config options no longer have effect:
            PidFile, LogFile.

--- pyzor/scripts/test.sh
+++ pyzor/scripts/test.sh
@@ -3,19 +3,49 @@
 # HOME so it finds the right .pyzor
 export HOME=.
 export PYTHONPATH=../lib
-PYZOR="./pyzor --homedir ."
-PYZOR_BOB="./pyzor --homedir bob"
-PYZORD="./pyzord --homedir ."
-alias check="$PYZOR check < test.in.0"
-alias check.bob="$PYZOR check < test.in.0"
+
+start_server()
+{
+    ./pyzord --homedir . > pyzord.log &
+    PYZORD_PID=$!
+    echo "started server with pid $PYZORD_PID"
+}
+
+
+pyzor()
+{
+    ./pyzor --homedir . "$@"
+}
+
+
+pyzor_bob()
+{
+    pyzor --homedir bob "$@"
+}
+
+
+check()
+{
+    pyzor check < test.in.0
+}
+
+
+check_bob()
+{
+    pyzor_bob check < test.in.0
+}
+
+
 
 kill_server()
 {
-  echo "killing server"
-  pidfile='pyzord.pid'
-  [ -e $pidfile ] && kill `cat $pidfile`
+    if [ ${PYZORD_PID:--1} != -1 ]; then
+	echo "killing server (pid $PYZORD_PID)"
+	kill $PYZORD_PID
+    else
+	"no server to kill"
+    fi
 }
-
 
 fail()
 {
@@ -26,24 +56,34 @@ fail()
 
 setcount()
 {
-  count=`$PYZOR check < test.in.0 | cut -f 3`
+    count=`pyzor check < test.in.0 | cut -f 3`
 }
 
 setcount_bob()
 {
-  count=`$PYZOR check < test.in.0 | cut -f 3`
+    count=`pyzor_bob check < test.in.0 | cut -f 3`
 }
 
 fail_cmp()
 {
-  fail "got $1; expected $2"
+    fail "got $1; expected $2"
 }
 
 rm -f pyzord.*
 
 echo "starting server"
-$PYZORD || fail
+start_server
 
+# sleep to give it time to start listening
+sleep 2
+
+[ ${PYZORD_PID:--1} != -1 ] || fail "we didn't get a process id for the server!"
+kill -0 $PYZORD_PID || fail "process is dead"
+
+echo "anonymous: pinging"
+pyzor ping || fail
+
+export PYZORD_PID=$!
 
 echo "anonymous: ensuring a count of 0 at start"
 setcount
@@ -54,19 +94,19 @@ check && fail
 echo "bob: ensuring a count of 0 at start"
 setcount_bob
 [ ${count:--1} = 0 ] || fail
-check.bob && fail
+check_bob && fail
 
 
 echo "anonymous: reporting"
-$PYZOR report < test.in.0 && fail
+pyzor report < test.in.0 && fail
 echo "anonymous: reporting"
-$PYZOR report < test.in.0 && fail
+pyzor report < test.in.0 && fail
 
 
 echo "bob: reporting"
-$PYZOR_BOB report < test.in.0 || fail
+pyzor_bob report < test.in.0 || fail
 echo "bob: reporting"
-$PYZOR_BOB report < test.in.0 || fail
+pyzor_bob report < test.in.0 || fail
 
 
 echo "anonymous: counting reports"
@@ -78,38 +118,38 @@ check || fail
 echo "bob: counting reports"
 setcount_bob
 [ ${count:--1} = 2 ] || fail_cmp ${count:--1} 2
-check.bob || fail "checking failed"
+check_bob || fail "checking failed"
 
 
 echo "bob: reporting a mailbox"
-$PYZOR_BOB report --mbox < test.in.mbox || fail
+pyzor_bob report --mbox < test.in.mbox || fail
 
 
 echo "bob: counting reports"
 setcount_bob
 [ ${count:--1} = 3 ] || fail_cmp ${count:--1} 3
-check.bob || fail "checking exit code failed"
+check_bob || fail "checking exit code failed"
 
 
 echo "bob: getting info"
 # check exit
-$PYZOR_BOB info < test.in.0 || fail
+pyzor_bob info < test.in.0 || fail
 # check lines
-[ `$PYZOR_BOB info < test.in.0 | wc -l` = 7 ] || fail
+[ `pyzor_bob info < test.in.0 | wc -l` = 7 ] || fail
 
 
 echo "anonymous: whitelisting"
-$PYZOR whitelist < test.in.0 && fail
+pyzor whitelist < test.in.0 && fail
 
 echo "bob: whitelisting"
-$PYZOR_BOB whitelist < test.in.0 || fail
+pyzor_bob whitelist < test.in.0 || fail
 
 
 echo "bob: getting info"
 # check exit
-$PYZOR_BOB info < test.in.0 || fail
+pyzor_bob info < test.in.0 || fail
 # check lines
-[ `$PYZOR_BOB info < test.in.0 | wc -l` = 7 ] || fail
+[ `pyzor_bob info < test.in.0 | wc -l` = 7 ] || fail
 
 
 echo "bob: counting reports"
@@ -118,16 +158,13 @@ setcount_bob
 check && fail
 
 
-echo "anonymous: pinging"
-$PYZOR ping || fail
-
 echo "checking for logfile"
 [ -s pyzord.log ] || fail
 
 echo "anonymous: shutting down server"
-$PYZOR shutdown 127.0.0.1:9999 && fail
+pyzor shutdown 127.0.0.1:9999 && fail
 
 echo "bob: shutting down server"
-$PYZOR_BOB shutdown 127.0.0.1:9999 || fail
+pyzor_bob shutdown 127.0.0.1:9999 || fail
 
 echo "passed"
