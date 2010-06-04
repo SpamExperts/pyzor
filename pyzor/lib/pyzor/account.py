d29b0af3e206b84029568bff09123a8b2782cfcb
Tony Meyer <tony@spamexperts.com>
2010-06-04 04:05:10 +0000

    Add simple functions that sign/verify a message.  This functionality is from the MacEnvelope class in __init__.py.  __init__.py isn't really the right place to hold this much code, and the code can be vastly simplier than it was.  The only real loss is typechecking - but we can have a good suite of unit tests that ensure that we use the methods correctly internally, and if anyone is using this as a library, then it is their responsiblity to use the methods appropriately (type checking is not very Pythonic).

--- pyzor/lib/pyzor/account.py
+++ pyzor/lib/pyzor/account.py
@@ -1,8 +1,63 @@
+"""A collection of utilities that facilitate working with Pyzor accounts.
+
+Note that accounts are not necessary (on the client or server), as an
+"anonymous" account always exists."""
+
 import os
+import time
+import hashlib
 import logging
 import collections
 
 import pyzor
+
+# This is the maximum time between a client signing a Pyzor request and the
+# server checking the signature.
+MAX_TIMESTAMP_DIFFERENCE = 300 # seconds
+
+def sign_msg(hashed_key, timestamp, msg, hash=hashlib.sha1):
+    """Converts the key, timestamp (epoch seconds), and msg into a digest.
+
+    lower(H(H(M) + ':' T + ':' + K))
+    M is message
+    T is decimal epoch timestamp
+    K is hashed_key
+    H is the hash function (currently SHA1)
+    """
+    return hash("%s:%d:%s" % (hash(str(msg)).digest(), timestamp,
+                              hashed_key)).hexdigest().lower()
+
+def hash_key(key, user, hash=hashlib.sha1):
+    """Returns the hash key for this username and password.
+
+    lower(H(U + ':' + lower(K)))
+    K is key (hex string)
+    U is username
+    H is the hash function (currently SHA1)
+    """
+    return hash("%s:%s" % (user, key.lower())).hexdigest().lower()
+
+def verify_signature(msg, user_key):
+    """Verify that the provided message is correctly signed.
+
+    The message must have "User", "Time", and "Sig" headers.
+
+    If the signature is valid, then the function returns normally.
+    If the signature is not valid, then a pyzor.SignatureError() exception
+    is raised."""
+    timestamp = int(request["Time"])
+    user = request["User"]
+    provided_signature = request["Sig"]
+    # Check that this signature is not too old.
+    if abs(time.time() - timestamp) > MAX_TIMESTAMP_DIFFERENCE:
+        raise SignatureError("Timestamp not within allowed range.")
+    # Calculate what the correct signature is.
+    hashed_user_key = hash_key(user_key, user)
+    # The signature is not part of the message that is signed.
+    del request["Sig"]
+    correct_signature = sign_msg(hashed_user_key, timestamp, msg)
+    if correct_signature != provided_signature:
+        raise pyzor.SignatureError("Invalid signature.")
 
 class Account(object):
     def __init__(self, username, salt, key):
