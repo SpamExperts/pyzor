f01a71f95270980056b562ecbc07fc0f5928cc26
Alexandru Chirila <alex@alexkiro.com>
2014-04-01 14:49:39 +0000

    Add classifiers to distutils setup.

--- pyzor/setup.py
+++ pyzor/setup.py
@@ -9,6 +9,25 @@ Pyzor is spam-blocking networked system that uses spam signatures
 to identify them.
 """
 
+classifiers = ["Operating System :: POSIX",
+               
+               "Environment :: Console",
+               "Environment :: No Input/Output (Daemon)",
+               
+               "Programming Language :: Python"
+               "Programming Language :: Python :: 2.6",
+               "Programming Language :: Python :: 3",
+               
+               "Intended Audience :: System Administrators",
+
+               "Topic :: Communications :: Email",
+               "Topic :: Communications :: Email :: Filters",
+
+               "Development Status :: 5 - Production/Stable",
+
+               "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+               ]
+
 distutils.core.setup(name='pyzor',
                      version=pyzor.__version__,
                      description='networked spam-signature detection',
@@ -21,5 +40,6 @@ distutils.core.setup(name='pyzor',
                      url='http://pyzor.sourceforge.net/',
                      scripts=['scripts/pyzor', 'scripts/pyzord'],
                      packages=['pyzor'],
+                     classifiers=classifiers,
                      test_suite="tests.suite",
                      )
