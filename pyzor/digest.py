1a0c7999d806e89ba05ddde00ef2439d1e335062
Alexandru Chirila <alex@alexkiro.com>
2014-06-10 16:39:40 +0300

    Quopri codec only supports strict errors.

--- pyzor/digest.py
+++ pyzor/digest.py
@@ -118,15 +118,21 @@ class DataDigester(object):
         for part in msg.walk():
             if part.get_content_maintype() == "text":
                 payload = part.get_payload(decode=True)
+
                 charset = part.get_content_charset()
+                errors = "ignore"
                 if not charset:
                     charset = "ascii"
+                elif (charset.lower().replace("_", "-") in ("quopri-codec", 
+                      "quopri", "quoted-printable", "quotedprintable")):
+                    errors = "strict"
+
                 try:
-                    payload = payload.decode(charset, "ignore")
-                except (LookupError, UnicodeDecodeError):
+                    payload = payload.decode(charset, errors)
+                except (LookupError, UnicodeError):
                     try:
                         payload = payload.decode("ascii", "ignore")
-                    except UnicodeDecodeError:
+                    except UnicodeError:
                         continue
                 if part.get_content_subtype() == "html":
                     yield cls.normalize_html_part(payload)
