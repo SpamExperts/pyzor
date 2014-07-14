22e2a3b141840ed3fa2e584aab299e89dd53e749
Alexandru Chirila <alex@alexkiro.com>
2014-07-14 21:35:59 +0300

    Bump version :point_up:

--- pyzor/__init__.py
+++ pyzor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Frank J. Tobin, ftobin@neverending.org"
 __credits__ = "Tony Meyer, Dreas von Donselaar, all the Pyzor contributors."
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 import hashlib
 
