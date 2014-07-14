2f9082d3b03a419291bcea16c61304100373acf0
Alexandru Chirila <alex@alexkiro.com>
2014-07-14 08:30:32 +0300

    Allow unix socket use with redis server.

--- pyzor/engines/redis_.py
+++ pyzor/engines/redis_.py
@@ -90,6 +90,9 @@ class RedisDBHandle(object):
 
     @safe_call
     def _get_new_connection(self):
+        if "/" in self.host:
+            return redis.StrictRedis(unix_socket_path=self.host,
+                                     db=int(self.db_name), password=self.passwd)
         return redis.StrictRedis(host=self.host, port=int(self.port),
                                  db=int(self.db_name), password=self.passwd)
 
