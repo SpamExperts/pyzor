b908b38302c44eb733f5d0fb01b50d6e5e914d4a
Alexandru Chirila <alex@alexkiro.com>
2016-01-15 13:03:23 +0200

    Refs. #46. Remove the 2to3 conversion.
    Remove from the setup script and the run_tests
    helper script.

--- setup.py
+++ setup.py
@@ -4,16 +4,6 @@ import distutils.core
 
 import pyzor
 
-try:
-    # These automatically run 2to3 and modules and scripts while installing,
-    # when ran under Python 3
-    from distutils.command.build_py import build_py_2to3 as build_py
-    from distutils.command.build_scripts import \
-        build_scripts_2to3 as build_scripts
-except ImportError:
-    from distutils.command.build_py import build_py
-    from distutils.command.build_scripts import build_scripts
-
 long_description = """
 Pyzor is spam-blocking networked system that uses spam signatures
 to identify them.
@@ -36,27 +26,26 @@ classifiers = ["Operating System :: POSIX",
 
                "Development Status :: 5 - Production/Stable",
 
-               "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-]
+               "License :: OSI Approved :: GNU General Public License v2 ("
+               "GPLv2)",
+               ]
 
-distutils.core.setup(name='pyzor',
-                     version=pyzor.__version__,
-                     description='networked spam-signature detection',
-                     long_description=long_description,
-                     author='Frank J. Tobin',
-                     author_email='ftobin@neverending.org',
-                     license='GPL',
-                     platforms='POSIX',
-                     keywords='spam',
-                     url='http://www.pyzor.org/',
-                     scripts=['scripts/pyzor', 'scripts/pyzord',
-                              'scripts/pyzor-migrate'],
-                     packages=['pyzor',
-                               'pyzor.engines',
-                               'pyzor.hacks'],
-                     classifiers=classifiers,
-                     test_suite="tests.suite",
-                     cmdclass={'build_py': build_py,
-                               'build_scripts': build_scripts,
-                     },
+distutils.core.setup(
+        name='pyzor',
+        version=pyzor.__version__,
+        description='networked spam-signature detection',
+        long_description=long_description,
+        author='Frank J. Tobin',
+        author_email='ftobin@neverending.org',
+        license='GPL',
+        platforms='POSIX',
+        keywords='spam',
+        url='http://www.pyzor.org/',
+        scripts=['scripts/pyzor', 'scripts/pyzord',
+                 'scripts/pyzor-migrate'],
+        packages=['pyzor',
+                  'pyzor.engines',
+                  'pyzor.hacks'],
+        classifiers=classifiers,
+        test_suite="tests.suite",
 )
