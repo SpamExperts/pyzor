f16e1b60728e8d08efd003c55d9f47dc298c3fe8
Alexey Anisimov <aragorna@gmail.com>
2016-01-27 13:01:53 -0600

    Refs. #38. Use io.open for compatibility

--- tests/functional/test_pyzor.py
+++ tests/functional/test_pyzor.py
@@ -1,3 +1,4 @@
+import io
 import sys
 import redis
 import unittest
@@ -128,8 +129,8 @@ class PyzorScriptTest(PyzorTestBase):
         self.assertEqual(r["WL-Count"], "1")
 
     def test_mbox_real(self):
-        with open(MBOX_FILE_PATH) as mbox_file:
-            input = mbox_file.read().decode("latin-1")
+        with io.open(MBOX_FILE_PATH, 'rt', encoding='latin-1') as mbox_file:
+            input = mbox_file.read()
             self.client_args["-s"] = "mbox"
             self.check_pyzor("pong", None, input=input, code=200, exit_code=0,
                              counts=(sys.maxsize, 0))
