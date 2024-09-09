a4c94a2c0878f80edff548e6f67f6846c484a3b8
Mugur Anghel <69189130+MugurAnghel@users.noreply.github.com>
2024-09-09 16:47:04 +0300

    Bump version (#172)
    * Py3 lint
    
    * Update changelog
    
    * Bump version
    
    * Bump version
    
    ---------
    
    Co-authored-by: Mugur Anghel <mugur.anghel@nable.com>

--- pyzor/__init__.py
+++ pyzor/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Frank J. Tobin, ftobin@neverending.org"
 __credits__ = "Tony Meyer, Dreas von Donselaar, all the Pyzor contributors."
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 import hashlib
 
